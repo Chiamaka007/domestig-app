641d229ac2124ba8072ed89a16a94e964c847ca9
Chiamaka Ugoji <mychiamaka@gmail.com>
2022-01-14 08:11:50 +0100

    added sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Dashboard from './pages/Dashboard';
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
+    <Sidebar />
     
-    <Dashboard/>
 
     
   );
